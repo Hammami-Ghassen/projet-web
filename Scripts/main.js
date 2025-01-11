6553aa57172e2c01058ad9156f6fd2b365a325af
Ghassen Hammami <ghassene.hammami@isgb.ucar.tn>
2025-01-11 15:58:47 +0100

    footer
    j'ai presque terminé le footer

--- Scripts/main.js
+++ Scripts/main.js
@@ -11,25 +11,6 @@ const swiper = new Swiper(".swiper", {
   },
 
 });
-/*const swiper = new Swiper('.swiper', {
-  slidesPerView: 3,  // Adjust for the number of visible cards
-  spaceBetween: 30,  // Space between slides
-  loop: true,        // Enable/disable looping as needed
-  centeredSlides: false, // Disable centering
-  initialSlide: 0,   // Ensure the first slide starts from the left
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  breakpoints: { // Make it responsive
-    640: { slidesPerView: 1, spaceBetween: 10 },
-    768: { slidesPerView: 2, spaceBetween: 20 },
-    1024: { slidesPerView: 3, spaceBetween: 30 },
-  },
-});
-*/
-
-
 //navbar toggler
 const button = document.getElementById('nav-toggler');
 button.addEventListener('click', () => {
